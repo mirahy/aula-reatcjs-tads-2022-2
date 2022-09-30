0a4b6fcdcde6f605024f9a292e1888dba2059bf1
Mirahy <mirahy@gmail.com>
2022-09-29 20:12:11 -0400

    Aula 29.09.2022

--- src/api/cep.ts
+++ src/api/cep.ts
@@ -1,13 +1,8 @@
 
-type CepProps = {
-    cep:string
-}
 
-export async function getCep({cep}:CepProps) {
+export async function getCep(cep:string) {
     try {
-
-        console.log('viacep.com.br/ws/'+{cep}+'/json/')
-    let response = await fetch('viacep.com.br/ws/'+{cep}+'/json/')
+    let response = await fetch(`viacep.com.br/ws/'${cep}'/json/`)
     let dados = await response.json()
     return dados
     } catch (error) {
--- src/components/FormUser/index.tsx
+++ src/components/FormUser/index.tsx
@@ -1,5 +1,5 @@
 import { KeyboardEvent, useEffect, useState } from "react";
-import { Col, Form, Row, Button } from "react-bootstrap";
+import { Col, Form, Row, Button, Alert } from "react-bootstrap";
 import { getCep } from "../../api/cep";
 import { FormUserStyle } from './styled';
 import { BeatLoader } from "react-spinners";
@@ -7,33 +7,38 @@ import { BeatLoader } from "react-spinners";
 
 export function FormUser() {
 
-    const [cep, setCep] = useState('')
+    const [cep, setCep] = useState("")
     const [isLoading, setIsLoading] = useState(false)
     const [endereco, setEndereco] = useState('')
 
 
-    function listAdress(cep){
-        const adress =  getCep(cep)
-        console.log(adress)
-        setEndereco(adress.logradouro)
+    const listAdress = async () =>{
+        const address = await getCep(cep)
+        if (address.hasOwnProperty('erro')) {
+            alert('<b>O CEP digitado não foi encontrado !</b>')
+        }
+        setEndereco(address.logradouro)
         setIsLoading(false)   
     }
 
     useEffect(()=>{
-         
+    
     },[])
     let text =''
     function handleKeyPress(event: KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) {
         
-        console.log(event.key);
         if (event.key === "Enter") {
             setIsLoading(true) 
-            console.log('ok')
-            setTimeout(listAdress(text), 2000);
+            console.log(text)
+            setCep(text)
+            setTimeout(listAdress, 3000);
           }else{
             text += event.key;
-            console.log(text)
           }
+      }
+
+      function handleChange(event: KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        console.log(event.target.value);
       }
 
     return (
@@ -71,13 +76,13 @@ export function FormUser() {
                     <Col>
                         <Form.Group className="mb-3" controlId="formBasicCEP">
                             <Form.Label column>CEP: </Form.Label>
-                            <Form.Control type="text" placeholder="CEP" onKeyPress={(event) => handleKeyPress(event)} />
+                            <Form.Control type="text" placeholder="CEP"  onChange={handleChange}/>
                         </Form.Group>
                     </Col>
                     <Col xs={8}>
                         <Form.Group className="mb-3" controlId="formBasicEndereco">
                             <Form.Label column>Endereço: </Form.Label>
-                            <Form.Control type="text" placeholder="Endereço" value={endereco} />
+                            <Form.Control type="text" placeholder="Endereço" value={endereco} onChange={()=>{}}/>
                         </Form.Group>
                     </Col>
                 </Row>
@@ -85,7 +90,7 @@ export function FormUser() {
                     <Col>
                         <Form.Group className="mb-3" controlId="formBasicCidade">
                             <Form.Label>Cidade: </Form.Label>
-                            <Form.Select aria-label="Default select example">
+                            <Form.Select aria-label="Default select example" >
                                 <option>Selecione a cidade</option>
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
@@ -96,7 +101,7 @@ export function FormUser() {
                     <Col>
                         <Form.Group className="mb-3" controlId="formBasicEstado">
                             <Form.Label>Estado: </Form.Label>
-                            <Form.Select aria-label="Default select example">
+                            <Form.Select aria-label="Default select example" >
                                 <option>Selecione o estado</option>
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
